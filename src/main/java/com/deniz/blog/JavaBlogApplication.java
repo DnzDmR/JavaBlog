e8954f3ad322d24faad955be26e19885bbb223f9
Denizcan Demir <denizcan.demir@ozguryazilim.com.tr>
2018-08-28 15:27:54 +0300

    temel kurulum

---
+++ src/main/java/com/deniz/blog/JavaBlogApplication.java
@@ -0,0 +1,12 @@
+package com.deniz.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBlogApplication.class, args);
+	}
+}
