052dfc34f17b6b6f66336de378e9de57fedfb4a9
Denizcan Demir <denizcan.demir@ozguryazilim.com.tr>
2018-08-29 23:31:06 +0300

    news icin view olusturuldu ve silme eklendi

--- src/main/java/com/deniz/blog/controller/NewsController.java
+++ src/main/java/com/deniz/blog/controller/NewsController.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -21,10 +22,9 @@ public class NewsController {
 
 	@Autowired
 	NewsRepository newsRepository;
-	
+
 	@Autowired
 	UsersRepository usersRepository;
-	
 
 	@RequestMapping(value = "/add", method = RequestMethod.GET)
 	public String getNewsAddPage(Model model) {
@@ -36,14 +36,33 @@ public class NewsController {
 	@RequestMapping(value = "/add", method = RequestMethod.POST)
 	public String postNewsAddPage(@ModelAttribute("news") News news) {
 
-		
 		String currentPrincipalName = SecurityContextHolder.getContext().getAuthentication().getName();
 
 		news.setAuthor(usersRepository.getUserByUsername(currentPrincipalName).get());
 		news.setCreateDate(new Date(Calendar.getInstance().getTime().getTime()));
 		newsRepository.save(news);
 
-		return "adminPages/newsEdit";
+		return "redirect:/admin/news/list";
 	}
+
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public String getNewsListPage(Model model) {
+
+		Iterable<News> news = newsRepository.findAll();
+
+		model.addAttribute("news", news);
+
+		return "adminPages/newsList";
+	}
+	
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String getNewsDelete(@PathVariable("id") Integer id) {
+		
+		newsRepository.deleteById(id);
+	 
+		return "redirect:/admin/news/list";
+	}
+	
+	
 
 }
