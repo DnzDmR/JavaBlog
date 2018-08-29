654fea97786b1f64ebe424f87e1d4c68241c28b1
Denizcan Demir <denizcan.demir@ozguryazilim.com.tr>
2018-08-29 14:58:20 +0300

    contact view tamamlandi

--- src/main/java/com/deniz/blog/controller/HomeController.java
+++ src/main/java/com/deniz/blog/controller/HomeController.java
@@ -6,14 +6,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("")
 public class HomeController {
 	
 	
-	@RequestMapping(value="", method= RequestMethod.GET)
+	@RequestMapping(value= {"","/"}, method= RequestMethod.GET)
 	public String getHomePage(Model model) {
 		
 		return "pages/home";
 	}
+	
+	@RequestMapping(value="/contact", method= RequestMethod.GET)
+	public String getContactPage(Model model) {
+		
+		return "pages/contact";
+	}	
 
 }
